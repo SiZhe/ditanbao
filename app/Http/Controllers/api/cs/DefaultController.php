e5f2af6507fa537831ea571d3c95e34c0a06cd47
郭会林 <huilin_guo@126.com>
2020-07-03 22:55:32 +0800

    用户版本切换

--- app/Http/Controllers/api/cs/DefaultController.php
+++ app/Http/Controllers/api/cs/DefaultController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\api\cs;
 
 use App\Http\Resources\UserResource;
 use App\Http\Resources\StallCollection;
+use Illuminate\Support\Facades\Request;
+use App\Models\User;
 
 class DefaultController extends BaseController {
     
@@ -14,5 +16,19 @@ class DefaultController extends BaseController {
 	public function stalls() {
 	    $stalls = $this->user->stalls()->paginate(30);
 	    return $this->respOK(new StallCollection($stalls));
+	}
+	
+	public function version() {
+	    $version = Request::input('version');
+	    if(!in_array($version, [User::VERSION_COMMON, User::VERSION_STALL])) {
+	        return $this->error(self::ERROR_PARAMETER);
+	    }
+	    $user = $this->user;
+	    if($version == User::VERSION_STALL and is_null($user->stall)) {
+	        return $this->error(self::ERROR_STALL_NOT_OPEN);
+	    }
+	    $user->version = $version;
+	    $user->save();
+	    return $this->respOK(new UserResource($user));
 	}
 }
